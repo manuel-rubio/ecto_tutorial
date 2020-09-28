18efaf9c3656c3d246c2a526b1a27a6dd8a27761
Manuel Rubio <manuel.rubio@erlang-solutions.com>
2020-09-28 23:50:49 +0200

    Changesets

--- lib/ecto_tutorial/customer.ex
+++ lib/ecto_tutorial/customer.ex
@@ -1,5 +1,6 @@
 defmodule EctoTutorial.Customer do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "customers" do
     field :first_name, :string
@@ -8,5 +9,19 @@ defmodule EctoTutorial.Customer do
     field :phone, :string
     field :preferred_contact, :string
     timestamps()
+  end
+
+  @required_fields ~w[
+    first_name
+    last_name
+    preferred_contact
+  ]a
+  @optional_fields ~w[ phone email ]a
+
+  @doc false
+  def changeset(model, params) do
+    model
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
   end
 end
--- lib/ecto_tutorial/customers.ex
+++ lib/ecto_tutorial/customers.ex
@@ -5,21 +5,21 @@ defmodule EctoTutorial.Customers do
   Retrieve a customer based on the provided ID.
   """
   def get(id) do
-    {:error, :noimpl}
+    Repo.get(Customer, id)
   end
 
   @doc """
   Retrieve a customer based on the provided last name.
   """
   def get_by_last_name(last_name) do
-    {:error, :noimpl}
+    Repo.get_by(Customer, last_name: last_name)
   end
 
   @doc """
   Retrieve all of the customers.
   """
   def get_all() do
-    {:error, :noimpl}
+    Repo.all(Customer)
   end
 
   @doc """
@@ -28,7 +28,12 @@ defmodule EctoTutorial.Customers do
   ok-tuple with the inserted structure for the customer.
   """
   def insert_by_phone(first_name, last_name, phone) do
-    {:error, :noimpl}
+    Repo.insert(%Customer{
+      first_name: first_name,
+      last_name: last_name,
+      phone: phone,
+      preferred_contact: "phone"
+    })
   end
 
   @doc """
@@ -37,6 +42,19 @@ defmodule EctoTutorial.Customers do
   ok-tuple with the inserted structure for the customer.
   """
   def insert_by_email(first_name, last_name, email) do
+    Repo.insert(%Customer{
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      preferred_contact: "email"
+    })
+  end
+
+  @doc """
+  Update the first name and last name for a customer given its ID.
+  It should return the Customer struct in an ok-tuple or an error.
+  """
+  def update(id, first_name, last_name) do
     {:error, :noimpl}
   end
 
@@ -45,6 +63,9 @@ defmodule EctoTutorial.Customers do
   removed entry or an error-tuple.
   """
   def delete(id) do
-    {:error, :noimpl}
+    case get(id) do
+      %Customer{} = customer -> Repo.delete(customer)
+      nil -> {:error, :notfound}
+    end
   end
 end
