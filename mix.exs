cf9db6571735dee6463745547771f5b80fcd60cf
Manuel Rubio <manuel.rubio@erlang-solutions.com>
2020-09-28 18:51:43 +0200

    initial commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule EctoTutorial.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ecto_tutorial,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {EctoTutorial.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+    ]
+  end
+end
